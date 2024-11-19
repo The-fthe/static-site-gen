d2219093043484ed30d0f6bf843304a707ae8aae
fthe <lcslianandfthe.cs@gmail.com>
2024-11-19 21:45:26 +0900

    test: added more html test

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,6 +4,21 @@ from htmlnode import HTMLNode
 
 
 class TestHtmlNode(unittest.TestCase):
+    def test_prop_to_tag(self):
+        node = HTMLNode("h1", None, None, None)
+        check_html = "h1"
+        self.assertEqual(node.tag, check_html)
+
+    def test_prop_to_value(self):
+        node = HTMLNode(None, "this is a good day to die", None, None)
+        check_html = "this is a good day to die"
+        self.assertEqual(node.value, check_html)
+
+    def test_prop_to_child(self):
+        htmlNode = HTMLNode("h1", "this is a google")
+        node = HTMLNode("h1", "this is a google", htmlNode)
+        self.assertEqual(node.children, htmlNode)
+
     def test_prop_to_html_1_set(self):
         dict = {"href": "https://www.google.com"}
         node = HTMLNode("", "", None, dict)
