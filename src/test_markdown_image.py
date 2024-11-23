d3016710c548b05b503838e36bca8f256fb75982
fthe <lcslianandfthe.cs@gmail.com>
2024-11-23 19:37:39 +0900

    feature: added markdown image and link extract to tuple

---
+++ src/test_markdown_image.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import (
+    extract_markdown_images,
+    extract_markdown_links
+)
+
+
+class TestMarkdownImage(unittest.TestCase):
+
+    def test_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        check = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                 ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extract_markdown_images(text), check)
+
+    def test_markdown_url(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        check = [("to boot dev", "https://www.boot.dev"),
+                 ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extract_markdown_links(text), check)
+
+    def test_markdown_url2(self):
+        text = "This is text with a link [to boot dev1](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        check = [("to boot dev1", "https://www.boot.dev"),
+                 ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extract_markdown_links(text), check)
+
+    if __name__ == "__main__":
+        unittest.main()
