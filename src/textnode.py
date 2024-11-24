61c1052c3888866592add12c37db9d9567b235c2
fthe <lcslianandfthe.cs@gmail.com>
2024-11-24 18:21:20 +0900

    feature: added text to textnode convert with test

--- src/textnode.py
+++ src/textnode.py
@@ -30,6 +30,14 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+    return nodes
 
 
 def text_node_to_html_node(text_node):
