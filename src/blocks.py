5dcf578490348c171a4ef4052151b56cc46b4240
fthe <lcslianandfthe.cs@gmail.com>
2024-11-29 00:01:38 +0900

    refactor: remove old block_to_block_type

--- src/blocks.py
+++ src/blocks.py
@@ -49,38 +49,3 @@ def block_to_block_type(block):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
-
-
-# def block_to_block_type(block):
-#     if len(block) > 2:
-#         # check for header
-#         if block[0] == '#' and \
-#                 block.count('#') and \
-#                 block[block.count('#')] == " ":
-#             return BlockType.HEADING
-#         # check for code
-#         if block.count('`') == 6 and \
-#                 block[:3].count('`') == 3 and \
-#                 block[:-3].count('`') == 3:
-#             return BlockType.CODE
-#         # check for quote
-#         if block[0] == '>':
-#             return BlockType.QUOTE
-#         # check for unorder list '*'
-#         if (block[0] == '*' or block[0] == '-') and block[1] == " ":
-#             return BlockType.UNORDERED_LIST
-#         # check for order list
-#         if block[0].isdigit() and block[1] == ".":
-#             lists = block.split('\n')
-#             if len(lists) == 1:
-#                 return BlockType.ORDERED_LIST
-#             for i in range(len(lists)):
-#                 if lists[i][0].isdigit() is False or lists[i][1] != ".":
-#                     return BlockType.PARAGRAPH
-#                 if i == 0:
-#                     continue
-#                 if int(lists[i-1][0])+1 != int(lists[i][0]) or lists[i][1] != ".":
-#                     return BlockType.PARAGRAPH
-#             return BlockType.ORDERED_LIST
-#
-#     return BlockType.PARAGRAPH
